9735c6a3841822b845a3f45f77b9dfb4258953b3
joaopavila120 <joaopaulo1448@gmail.com>
2024-11-20 17:43:08 -0300

    adicionada verificacao de imagem pra vers e é retina

--- image_processing.py
+++ image_processing.py
@@ -11,3 +11,15 @@ def analisar_retina(imagem_caminho):
     img_suave = cv2.GaussianBlur(img_equalizada, (5, 5), 0)
     
     return img_original, img, img_equalizada, img_suave
+
+def verificar_imagem_retina(img):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    bordas = cv2.Canny(img_gray, 50, 150)
+    proporcao_red = np.sum(img[:, :, 2] > 100) / (img.shape[0] * img.shape[1])
+
+    contornos, _ = cv2.findContours(bordas, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    areas_circulares = sum(
+        1 for contorno in contornos if cv2.arcLength(contorno, True) > 0
+    )
+
+    return areas_circulares >= 1 and proporcao_red > 0.01
